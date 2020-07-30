515926727c76568bf720d1cea59a344cfb68827a
Cade Brown <brown.cade@gmail.com>
2020-07-29 21:42:13 -0400

    vibed the gen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,36 @@ import Snare from './snare';
  * Each one represents one entire scale through
  */
 const scaleMidi = {
-    'major': [0, 2, 4, 5, 7, 9, 11],
-    'minor': [0, 2, 3, 5, 7, 8, 10],
+    'major':  [0, 2, 4, 5, 7, 9, 11],
+    'minor':  [0, 2, 3, 5, 7, 8, 10],
 };
 
-/*
+
+
+/* A list of 'chords' as MIDI
  *
  *
  */
-const chordProgs = {
-    "I-V-vi-IV": [
-        scaleMidi.major.map(i => i + 0),
-        scaleMidi.major.map(i => i + 7),
-        scaleMidi.major.map(i => i + 5),
-        scaleMidi.major.map(i => i + 9),
-    ]
+const chordMidi = {
+    'major':   [0, 4, 7],
+    'minor':   [0, 3, 7],
+    'major7':  [0, 4, 7, 11],
+    'minor7':  [0, 3, 7, 10],
+    '7':       [0, 4, 7, 10],
+    'aug':     [0, 4, 8],
+    'dim':     [0, 3, 6],
+    'dim7':    [0, 3, 6, 9],
+    'hdim7':   [0, 3, 6, 10],
 };
 
+// apply an offset to the array
+function withOffset(offset, arr) {
+    return arr.map(i => i + offset);
+}
 
 // calculate the 'hz' from a given midi note
 // For reference: middle C is 60
+// Allows fractional notes as well
 function fromMIDI(midiNote) {
     return Math.pow(2, (midiNote - 69) / 12) * 440;
 }
@@ -39,6 +49,39 @@ function fromMIDI(midiNote) {
 function getRandom(list, rngFunc) {
   return list[Math.floor(rngFunc() * list.length)];
 }
+
+// rotate array by N, add add `vshift` to the shifted elements
+// Adds `vshift` multiple times if `n` caused the shift to wrap around the array
+function rot(arr, n=0, vshift=0) {
+    var new_arr = arr.map((elem, i) => arr[((i + n) % arr.length + arr.length) % arr.length]);
+    if (n > 0) {
+        for (var i = 0; i < n; ++i) new_arr[arr.length - i - 1] += vshift;
+    } else {
+        for (var i = 0; i < -n; ++i) new_arr[i] += vshift;
+    }
+    // if things were shifted more than an entire length, 
+    //   add multiple 'vshift' values to them
+    for (var i = 0; i < new_arr.length; ++i) {
+        new_arr[i] += vshift * Math.floor(Math.abs(n) / new_arr.length);
+    }
+
+    return new_arr;
+}
+
+
+
+/* List of common chord progressions
+ *
+ * TODO: add a programmatic parser to parse them and generate them
+ *
+ */
+const chordProgs = {
+    //"dsad": [[0, 'dim7'], [7, 'dim7'], [0, 'dim7'], [7, 'dim7']],
+    //"iii-iv-iii-I#": [[5, 'minor'], [6, 'minor'], [5, 'minor'], [1, 'major']],
+    "Imajor7-VIImajor-III7-VImajor7": [[0, 'major7'], [-1, 'major'], [4, '7'], [-3, 'major7']],
+    //"Imajor7-II7-iiminor7-V7": [[0, 'major7'], [2, '7'], [2, 'minor7'], [7, '7']],
+};
+
 
 /* Vibe - a 'Vibe' is basically a datastructure describing a song/generation routine
  *
@@ -67,6 +110,9 @@ class Vibe {
     // the loop that always plays
     loop;
 
+    // what iteration are we on?
+    iterN;
+
 
     constructor(seed) {
         var self = this;
@@ -74,6 +120,10 @@ class Vibe {
         self.seed = seed;
         self.RNG = SeedRandom(seed);
 
+        // start off on iteration 0
+        self.iterN = 0
+
+        // dictionary of instuments
         self.inst = {
 
             'Kick': new Tone.MembraneSynth().toMaster(),
@@ -106,7 +156,6 @@ class Vibe {
                 }
             }).toMaster(),
 
-
             'MainVoice':  new Tone.Synth().toMaster(),
 
             'ChordSupport': new Tone.PolySynth(8, Tone.AMSynth, {
@@ -116,55 +165,120 @@ class Vibe {
             }).toMaster(),
         };
 
+        // number of bars to generate per loop
+        var barsPerLoop = 4;
+        
+        // base MIDI noet
         var midiBase = 60;
 
-        var barsPerLoop = 4;
-        console.log("starting loop!")
+        // what chord should we use?
+        // TODO: change this over time
+        //const prog = "Imajor7-II7-iiminor7-V7";
+        const prog = getRandom(Object.keys(chordProgs), self.RNG);
 
+        console.log(prog)
+
+        // looping function to generate 
         self.loop = new Tone.Loop(function(time) {
+            console.log("LOOP: #" + self.iterN);
 
-            const chords = chordProgs["I-V-vi-IV"];
-            console.log("LOOP");
+
+            //
+            const chords = chordProgs[prog];
 
             for (var bar = 0; bar < barsPerLoop; ++bar) {
-                
-                // start time for the bar
+                // the start time for this bar                
                 var stime = time + Tone.Time("1:0:0") * bar;
-                var chord = chords[bar];
 
-                // loop through chord
-                /*([0, 2, 4]).forEach(i => {
-                    self.inst.ChordSupport.triggerAttackRelease(fromMIDI(midiBase + chord[i]), "4n", stime);
-                });*/
+                // get the current chord for this bar
+                var chord = chords[bar % chords.length];
 
+                /* DRUMS */
+
+                // Basic break-beat style pattern
+                // TODO: drum permutations and non-4/4 generation
                 for (var beat = 0; beat < 4; ++beat) {
                     self.inst.Kick.triggerAttackRelease('C1', "8n", stime + Tone.Time("4n") * beat);
                 }
                 for (var beat = 1; beat < 4; beat += 2) {
                     self.inst.Snare.triggerAttackRelease("8n", stime + Tone.Time("4n") * beat);
                 }
-
                 for (var beat = 0; beat < 4; ++beat) {
                     self.inst.OpenHat.triggerAttackRelease("8n", stime + Tone.Time("8n") * (2 * beat + 1));
                 }
 
+                /* MELODIES/BASSLINES */
+
+                // offset that the noets are applied with
+                var offset = midiBase + +chord[0];
+
+                // Generate the notes of the given scale
+                var notes = chordMidi[chord[1]];
+
+                //chord
+                self.inst.ChordSupport.triggerAttackRelease(fromMIDI(offset + notes[0] - 24), "4n", stime);
+
+
+
+                // inversion cycles;
+                // value is by how many notes the array is shifted
+                const cycl = [
+                    0,
+                    -1,
+                    0,
+                    -2,
+
+                    0,
+                    -1,
+                    1,
+                    2,
+                ];
+
+                // get current cycle
+                var this_cycl = cycl[self.iterN % cycl.length];
+
+                // get rotated notes, essentially an inversion of the existing ones in the
+                //   chord
+                var rot_notes = rot(notes, this_cycl, Math.sign(this_cycl) * 12);
+
+                // keep track of the last note played
+                var lastNote = -1;
+
                 for (var beat = 0; beat < 8; ++beat) {
-                    var curNote = midiBase + chord[getRandom([0, 2, 4, 5, 6], self.RNG)] - 12;
-                    self.inst.MainVoice.triggerAttackRelease(fromMIDI(curNote), "4n", stime + Tone.Time("8n") * beat);
+
+                    // generate random new note
+                    var curNote = offset + getRandom(rot_notes, self.RNG);
+                    if (curNote != lastNote) {
+                        // play note if it differs
+                        self.inst.MainVoice.triggerAttackRelease(fromMIDI(curNote), "4n", stime + Tone.Time("8n") * beat / 2);
+                        lastNote = curNote;
+                    } else {
+                        // otherwise set to -1, so that only a maximum of 1 note is skipped
+                        // i.e. if we didn't do this, then if we generated `AAA` it would just play `A`
+                        //   while we want it to play `A A`
+                        lastNote = -1;
+                    }
                 }
+
+
+
             }
 
+            // incremement iteration
+            self.iterN++;
 
         }, `${barsPerLoop}:0:0`);
     }
 
     // start the playback
     start() {
+        console.log("LOOP: start");
         this.loop.start(0);
     }
 
     // stop the playback
     stop() {
+        console.log("LOOP: stop");
         this.loop.stop(0);
     }
 
@@ -194,17 +308,16 @@ export class AppComponent {
     // called when it's created
     constructor() {
 
-
-
-
         // construct a generator
-        this.vibe = new Vibe('apple');
-
-        // start the vibe
-        this.vibe.start();
+        this.vibe = new Vibe('gregsucks');
 
         // begin the transport (for ToneJS)
         Tone.Transport.start();
+
+        Tone.Transport.bpm.value = 72;
+
+        // start the vibe
+        this.vibe.start();
 
     }
 }
